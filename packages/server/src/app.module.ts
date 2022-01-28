f34aa402502be86b7520d382a0f11348cc24fcfb
ryosuke mizuno <ryo.mizuno502@gmail.com>
2022-01-29 02:28:02 +0900

    [add] titleモデル、リゾルバーを作成

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {GraphQLModule} from "@nestjs/graphql";
+import { join } from 'path'
+import { TitlesModule } from './titles/titles.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: true,
+      sortSchema: true
+    }),
+    TitlesModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ packages/server/src/titles/titles.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { TitlesResolver } from './titles.resolver';
+import { TitlesService } from './titles.service';
+
+@Module({
+  providers: [TitlesResolver, TitlesService]
+})
+export class TitlesModule {}
