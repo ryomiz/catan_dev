35e717cfe8b2e9fee8848790962696bb38d6af19
ryosuke mizuno <ryo.mizuno502@gmail.com>
2022-01-30 00:06:05 +0900

    [add] Apollo Clientの設定

--- packages/frontend/src/pages/_app.tsx
+++ packages/frontend/src/pages/_app.tsx
@@ -1,9 +1,14 @@
-import '../styles/globals.css'
-
+import { ApolloProvider } from '@apollo/client'
 import type { AppProps } from 'next/app'
 
+import { client } from '../lib/apollo/client'
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
 }
 
 export default MyApp
