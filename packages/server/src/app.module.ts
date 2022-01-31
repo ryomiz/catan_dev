1e2819c5976f71946cfd8a4befc25e72bc071367
ryosuke mizuno <ryo.mizuno502@gmail.com>
2022-02-01 01:33:07 +0900

    [update] typeormを設定

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -6,12 +6,23 @@ import { join } from 'path';
 import { TitlesModule } from './titles/titles.module';
 import { UsersModule } from './users/users.module';
 import { GamesModule } from './games/games.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
       autoSchemaFile: join(process.cwd(), '../../schema.gql'),
       sortSchema: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'd056c35c973f',
+      port: 3306,
+      username: 'root',
+      password: 'password',
+      database: 'root',
+      entities: [],
+      synchronize: true,
     }),
     TitlesModule,
     UsersModule,
